85e1d465e438c7954d98f08f6be6b9e6f8e999ac
Marco Luz <marcotdl@hotmail.com>
2022-12-02 21:40:15 +0000

    Initial Classes

---
+++ src/ElevatorCall.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class ElevatorCall
+{
+    private String currentFloor;
+    private String direction;
+    private String destination;
+
+    public ElevatorCall(String currentFloor, String direction, String destination) {
+        this.currentFloor = currentFloor;
+        this.direction = direction;
+        this.destination = destination;
+    }
+
+    @Override
+    public String toString() {
+        return "ElevatorCall{" +
+                "currentFloor=" + currentFloor +
+                ", direction='" + direction + '\'' +
+                ", destination=" + destination +
+                '}';
+    }
+}
