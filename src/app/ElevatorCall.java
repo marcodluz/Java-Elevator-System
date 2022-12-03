957abbe91367b294bbbdfd7792c6ff5eee296508
Marco Luz <marcotdl@hotmail.com>
2022-12-03 12:09:59 +0000

    + ElevatorCall created + ElevatorCallTest created

--- src/app/ElevatorCall.java
+++ src/app/ElevatorCall.java
@@ -1,17 +1,70 @@
 package app;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ElevatorCall
 {
-    private String currentFloor;
-    private String direction;
-    private String destination;
+    public static int id = 0;
+    private int currentFloor;
+    private int direction;
+    private List<Integer> destinations = new ArrayList<>();
 
-    public ElevatorCall(String currentFloor, String direction, String destination) {
-        this.currentFloor = currentFloor;
-        this.direction = direction;
-        this.destination = destination;
+    public ElevatorCall() {
+
+    }
+
+    public void getNextFileCall(String file) {
+
+        FileReaderCSV fileReaderCSV = new FileReaderCSV(file);
+        fileReaderCSV.readFile();
+
+        int lineSize = fileReaderCSV.lines.get(id).size();
+        String next;
+        int idDestinations = 0;
+
+        System.out.println("Call ID #" + id);
+
+        for(int i = 0; i < lineSize; i++) {
+            if(i == 0) {
+                next = fileReaderCSV.lines.get(id).get(0);
+
+                if(next == "NOCALL") {
+                    break;
+                }
+
+                currentFloor = Integer.parseInt(next);
+
+            } else if (i == 1) {
+                next = fileReaderCSV.lines.get(id).get(1);
+                if (next.equals("DOWN")) {
+                    direction = 0;
+                } else if (next.equals("UP")) {
+                    direction = 1;
+                }
+
+            } else {
+
+                next = fileReaderCSV.lines.get(id).get(i);
+                destinations.add(Integer.parseInt(next));
+
+                //System.out.println("Destination #" + idDestinations + ": " + next);
+            }
+        }
+
+        id += 1;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public List<Integer> getDestinations() {
+        return destinations;
     }
 
     @Override
@@ -19,7 +72,7 @@ public class ElevatorCall
         return "ElevatorCall{" +
                 "currentFloor=" + currentFloor +
                 ", direction='" + direction + '\'' +
-                ", destination=" + destination +
+                ", destination=" + destinations +
                 '}';
     }
 }
--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        FileReaderCSV fileReaderCSV = new FileReaderCSV("src/db/CallList1.csv");
+        fileReaderCSV.readFile();
+        fileReaderCSV.listFileContents();
     }
 }
